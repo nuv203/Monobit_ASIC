578806ffe3a4a546145566b8a98b26be96b67b1e
JoshuaCho2025 <145054637+JoshuaCho2025@users.noreply.github.com>
2024-12-07 18:40:53 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -3,6 +3,7 @@
 /* This testbench just instantiates the module and makes some convenient wires
    that can be driven / tested by the cocotb test.py.
 */
+
 module tb ();
 
   // Dump the signals to a VCD file. You can view it with gtkwave.
@@ -12,29 +13,27 @@ module tb ();
     #1;
   end
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-   reg [7:0] ui_in;
-   reg [7:0] uo_out;
-  reg [7:0] uio_in;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+// Wire up the inputs and outputs:
+reg clk;
+reg rst_n;
+reg ena;
+reg [7:0] ui_in;
+reg [7:0] uo_out;
+reg [7:0] uio_in;
+wire [7:0] uio_out;
+wire [7:0] uio_oe;
+
 `ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
+wire VPWR = 1'b1;
+wire VGND = 1'b0;
 `endif
 
-  // Replace tt_um_example with your module name:
-  tt_um_monobit user_project (
-
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
-
+// Replace tt_um_example with your module name:
+tt_um_monobit user_project (
+      `ifdef GL_TEST
+         .VPWR(VPWR),
+         .VGND(VGND),
+      `endif
       .ui_in (ui_in),    // Dedicated inputs
       .uo_out(uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
