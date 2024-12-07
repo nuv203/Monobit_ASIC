f17839fe157f48d34b993ff4ad37ac26818b3c5f
JoshuaCho2025 <145054637+JoshuaCho2025@users.noreply.github.com>
2024-12-07 18:44:44 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -159,17 +159,6 @@ endmodule
 // ------------------------------------------------------------------
 
 
-module tt_um_monobit (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-);
-
 module monobit (
   clk, rst, is_random_rsc_dat, is_random_triosy_lz, valid_rsc_dat, valid_triosy_lz,
       epsilon_rsc_dat, epsilon_triosy_lz
